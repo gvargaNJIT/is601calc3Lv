4aaa9edbd94e98761113c46c30a47dc5565195af
gvargaNJIT <gev3@njit.edu>
2025-02-13 17:03:33 -0500

    Added Faker for more randomly generated tests

---
+++ IS601/calculator3Lv/tests/conftest.py
@@ -0,0 +1,45 @@
+'''Faker Python Test'''
+
+from decimal import Decimal
+from faker import Faker
+from calculator.operations import add, subtract, multiply, divide
+
+fake = Faker()
+
+def generate_test_data(num_records):
+    '''Having strings match operations in a set'''
+    operation_mappings = {
+        'add' : add,
+        'subtract' : subtract,
+        'multiply' : multiply,
+        'divide': divide
+    }
+
+    for _ in range(num_records):
+        a = Decimal(fake.random_number(digits=2))
+        b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
+        operation_name = fake.random_element(elements = list(operation_mappings.keys()))
+        operation_func = operation_mappings[operation_name]
+
+        if operation_func is divide:
+            b = Decimal('1') if b == Decimal('0') else b
+        try:
+            if operation_func is divide and b == Decimal('0'):
+                expected = "Undefined\n"
+            else:
+                expected = operation_func(a,b)
+        except ZeroDivisionError:
+            expected = "Undefined\n"
+        yield a, b, operation_name, operation_func, expected
+
+def pytest_addoption(parser):
+    '''Setting text instructions of when a faker test is generated with pytest'''
+    parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
+
+def pytest_generate_tests(metafunc):
+    '''Setting parameters for the faker pytest'''
+    if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
+        num_records = metafunc.config.getoption("num_records")
+        parameters = list(generate_test_data(num_records))
+        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in parameters]
+        metafunc.parametrize("a,b,operation,expected", modified_parameters)
--- IS601/calculator3Lv/tests/test_calculation.py
+++ IS601/calculator3Lv/tests/test_calculation.py
@@ -1,21 +1,10 @@
 '''Calculation Definition Test'''
 
-import pytest
+
 from calculator.calculation import Calculation
-from calculator.operations import add, subtract, multiply, divide
 
-@pytest.mark.parametrize("a_, b_, operation, expected", [
-    (4, 3, add, 7),
-    (7, 2, subtract, 5),
-    (6, 3, multiply, 18),
-    (81, 9, divide, 9),
-    (5.5, 4.5, add, 10),
-    (7.5, 4.3, subtract, 3.2),
-    (1.5, 2.0, multiply, 3.0),
-    (3.5, 1.75, divide, 2),
-])
 
-def test_calculations(a_, b_, operation, expected):
+def test_calculations(a, b, operation, expected):
     '''Testing all calculations'''
-    test = Calculation(a_, b_, operation)
+    test = Calculation(a, b, operation)
     assert test.get_result() == expected
